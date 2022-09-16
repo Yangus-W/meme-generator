b1d293af940b341fc3d00515a788e580a140ddf7
Yang Wang <wangyang.felix@hotmail.com>
2022-09-16 23:01:48 +0200

    Style form component

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,11 @@
+import "./Form.css"
+
 function Form() {
     return (
         <form className="form">
-            <div className='form--input'>
-                <input type='text'/>
-                <input type='text'/>
+            <div className="form--input">
+                <input className="form--input_child" type="text"/>
+                <input className="form--input_child" type='text'/>
             </div>
             <button className="form--button">Get a new meme image 🖼</button>
         </form>
